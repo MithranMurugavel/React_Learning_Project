b017624036ebcbf3fe583a616b4cc08146ef62fa
MithranMurugavel <mithran7741@gmail.com>
2026-05-17 00:28:54 +0530

    feat:Scroll Percentage

---
+++ assest/ScrollProgress/index.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react'
+import './style.css'
+const ScrollProgress = ({ url }) => {
+    const [percentage, setPercentage] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [data, setdata] = useState([]);
+    const [errmessage, setErrMsg] = useState("");
+    useEffect(() => {
+
+        async function fetchData(url) {
+            try {
+                setLoading(true);
+                const response = await fetch(url);
+                const info = await response.json();
+
+                if (info && info.products.length > 0)
+                    setdata(info.products);
+                setLoading(false);
+
+
+            }
+            catch (err) {
+                setLoading(false);
+                setErrMsg("Invalid URL");
+            }
+        }
+        fetchData(url);
+
+    }, [url])
+
+     useEffect(()=>{
+        window.addEventListener("scroll",handleScrollEvent);
+
+        return() =>{
+            window.removeEventListener("scroll",handleScrollEvent);
+        };
+        
+    },[])
+
+    if(errmessage){
+        return <div>Opps !! Error Occured</div>
+    }
+    if (loading) {
+        return <div>Loading data! Please wait</div>
+    }
+
+    function handleScrollEvent(){
+        const val = document.documentElement.scrollTop;
+        const val1 = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+        setPercentage((val/val1)*100);
+    }
+   
+   return (
+    <div>
+
+        <div className="top">
+
+            <div className="top-container">
+                <h1 className="banner">Product List</h1>
+
+                <div className="progress-bar">
+                    <div
+                        className="current-progress-bar"
+                        style={{ width: `${percentage}%` }}
+                    ></div>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="listItems">
+
+            {
+                data && data.length > 0
+                    ? data.map((dataItems) => (
+                        <p key={dataItems.id}>
+                            {dataItems.title}
+                        </p>
+                    ))
+                    : null
+            }
+
+        </div>
+
+    </div>
+)
+}
+
+export default ScrollProgress
--- assest/config/buttons_data.jsx
+++ assest/config/buttons_data.jsx
@@ -1,14 +1,17 @@
 import Accordian from "../Accordian";
 import ColorPanel from "../ColorChangePannel";
 import Qrcode from "../Qr_Code_Generator";
+import ScrollProgress from "../ScrollProgress";
 import StarRating from "../StarRating";
 import TreeView from "../Tree_View";
 import menus from "../Tree_View/data";
 export const buttons = [
   { id: "Accordian", label: "Accordian", component: <Accordian/> },
   { id: "Colour", label: "Colour", component: <ColorPanel/> },
-  { id: "StarRating", label: "Starrating", component: <StarRating value={10}/> },
-  { id: "TreeView", label: "MenuTreeView", component: <TreeView menu={menus}/> },
-  { id: "QRGenerator", label: "QrGenerator", component: <Qrcode/> },
+  { id: "StarRating", label: "Star Rating", component: <StarRating value={10}/> },
+  { id: "TreeView", label: "Menu Tree View", component: <TreeView menu={menus}/> },
+  { id: "QRGenerator", label: "QR Generator", component: <Qrcode/> },
+  { id: "ScrollPercentage", label: "Scroll Percentage", component: <ScrollProgress url={"https://dummyjson.com/products?limit=100"}/> },
+
 
 ];
